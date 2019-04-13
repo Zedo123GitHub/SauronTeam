c4c19bf10bb6807e3d63242817f24a6aed58d917
dante304404 <dante.3.kd@gmail.com>
2019-04-13 03:00:02 +0200

    Add recipe class and some path to data

--- ConsoleApp1/Learn.cs
+++ ConsoleApp1/Learn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.ML;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,39 @@ namespace ConsoleApp1
 {
     class Learn
     {
+        /// <summary>
+        /// Ścieżka do danych na których program będzie trenował.
+        /// </summary>
+        static readonly string _trainDataPath = System.IO.Path.Combine(Environment.CurrentDirectory, "Data", "data-train.csv");
+        /// <summary>
+        /// Ścieżka do danych na których program będzie testował czy dobrze się wyuczył.
+        /// </summary>
+        static readonly string _testDataPath = System.IO.Path.Combine(Environment.CurrentDirectory, "Data", "data-test.csv");
+        /// <summary>
+        /// Ścieżka w której znajduję się model.
+        /// </summary>
+        static readonly string _modelPath = System.IO.Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
 
+        public static void InitiateLearn()
+        {
+            Console.WriteLine("===== Rozpoczęto uczenie się =====");
+            MLContext mlContext = new MLContext(seed: 0);
+            // var model = Train(mlContext, _trainDataPath);
 
+            //  Evaluate(mlContext, model);
+
+            //  TestSinglePrediction(mlContext);
+            Console.WriteLine("===== Zakończono uczenie się =====");
+        }
+
+        /*
+        public static ITransformer Train(MLContext mlContext, string dataPath)
+        {
+          
+           // var model = pipeline.Fit(dataView);
+           // SaveModelAsFile(mlContext, model);
+           // return model;
+        }
+        */
     }
 }
---
+++ ConsoleApp1/Recipe.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Recipe
+    {
+        [LoadColumn(0)]
+        public string Test1;
+
+        [LoadColumn(1)]
+        public string Test2;
+
+        [LoadColumn(2)]
+        public float Test3;
+
+        [LoadColumn(3)]
+        public float Test4;
+
+        [LoadColumn(4)]
+        public float Test5;
+
+        [LoadColumn(5)]
+        public string BulkDensity;
+
+        [LoadColumn(6)]
+        public float Moisture;
+    }
+
+    class OutputPrediction
+    {
+        [ColumnName("BulkDensity")]
+        public string BulkDensity;
+
+        [ColumnName("Moisture")]
+        public float Moisture;
+    }
+}
