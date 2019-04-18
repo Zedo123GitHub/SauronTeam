ab65db4854946435f62eb28d93a5870281b2108d
ZedoBitbucket <zbynzet@gmail.com>
2019-04-18 23:30:19 +0200

    dodanie kilku projektów: - dao - dostep do danych - domain - jeden model aby rządzić pozostałymi projektami - okienka WinForms i WPF do testowania grafik

---
+++ DAO/DAO/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+	/// <summary>
+	/// zapewnienie komunikacji pomiedzy bazami danych lub innymi źrudłami danych (dla nas pliki csv i przerobienie ich na listy obiektów)
+	/// </summary>
+    public class Class1
+    {
+    }
+}
---
+++ Domain/Domain/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+	/// <summary>
+	/// zapewnienie jedenego wspólnego modelu dla wszystkich projektów w solucji
+	/// </summary>
+    public class Class1
+    {
+
+    }
+}
